2d85a540d8503db70d2fe4ffcb7e27d7028e60f0
noah <kawaii-93@mail.ru>
2025-07-27 14:46:48 +0300

    5_hw.py

--- 5_hw.py
+++ 5_hw.py
@@ -6,11 +6,11 @@ driver.get('https://www.saucedemo.com/')
 
 username = driver.find_element(By.ID, 'user-name')
 password = driver.find_element(By.ID, 'password')
-login_button = driver.find_element(By.ID, 'login-button')
+login_button = driver.find_element(By.ID, 'login-button') # в условии задачи кнопка называется submit, но я переименовала в login (<input type="submit" class="submit-button btn_action" data-test="login-button" id="login-button" name="login-button" value="Login">). надеюсь, так можно было))
 
 if username and password and login_button:
     print('элементы найдены')
 else:
     print('ошибка: какие-то элементы отсутствуют')
 
-driver.quit()
+driver.quit() # гугол подсказал, что quit можно добавить для закрытия браузера и завершения сессии webdriver
